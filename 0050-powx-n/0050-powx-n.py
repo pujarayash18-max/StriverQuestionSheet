039abb5703eb1dbeb2a22ba90cc62cce0441eed5
pujarayash18-max <pujarayash18@gmail.com>
2026-07-03 08:30:05 +0530

    Time: 0 ms (100%), Space: 12.3 MB (92.9%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,25 @@
+class Solution:
+    def myPow(self, x, n):
+
+        if n == 0:
+            return 1
+
+        if n < 0:
+            x = 1 / x
+            n = -n
+
+        result = 1
+
+        while n > 0:
+
+            if n % 2 == 1:
+                result *= x
+
+            x *= x
+            n //= 2
+
+        return result
+x = 2.00000
+n = 10
+sol = Solution()
+print(sol.myPow(x, n))
