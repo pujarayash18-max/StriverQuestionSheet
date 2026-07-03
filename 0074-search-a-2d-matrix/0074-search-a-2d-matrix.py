29c73b8ca24ee8232c3b0c1b6f00b6d8c3500bd0
pujarayash18-max <pujarayash18@gmail.com>
2026-07-03 08:27:54 +0530

    Time: 0 ms (100%), Space: 12.6 MB (30.41%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -20,3 +20,7 @@ class Solution:
                 right = mid - 1
 
         return False
+matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
+target = 4
+sol = Solution()
+print(sol.searchMatrix(matrix, target))
