f3661112a1b2e07f69932e14802e5e2464258154
pujarayash18-max <pujarayash18@gmail.com>
2026-08-22 10:56:59 +0530

    Time: 62 ms (73.92%), Space: 21.4 MB (25.54%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,12 +1,14 @@
 class Solution:
     def maxSubArray(self, nums):
-        current = maximum = nums[0]
+        maximum = nums[0]
+        total = 0
 
-        for i in range(1, len(nums)):
-            current = max(nums[i], current + nums[i])
-            maximum = max(maximum, current)
+        for i in range(len(nums)):
+            total = total + nums[i]
+
+            maximum = max(maximum, total)
+
+            if total < 0:
+                total = 0
 
         return maximum
-obj=Solution()
-nums=[-2,1,-3,4,-1,2,1,-5,4]
-print(obj.maxSubArray(nums))
