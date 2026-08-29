9a6a809dd50536122ad2405457a385c4bdb95f4c
pujarayash18-max <pujarayash18@gmail.com>
2026-08-29 09:43:29 +0530

    Time: 5 ms (11.93%), Space: 14.3 MB (14.79%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -1,27 +1,52 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution(object):
+class Solution:
     def reverseKGroup(self, head, k):
-        dummy = ListNode(0)
-        dummy.next = head
-        group_prev = dummy
-        while True:
-            kth = group_prev
-            for _ in range(k):
-                kth = kth.next
-                if not kth:
-                    return dummy.next
-            group_next = kth.next
-            prev = group_next
-            curr = group_prev.next
-            while curr != group_next:
-                temp = curr.next
-                curr.next = prev
-                prev = curr
-                curr = temp
-            temp = group_prev.next
-            group_prev.next = kth
-            group_prev = temp
+        
+        def getKthNode(node, k):
+            while node and k > 1:
+                node = node.next
+                k -= 1
+            return node
+
+        temp = head
+        prev = None
+
+        while temp:
+            # Find kth node of current group
+            kth = getKthNode(temp, k)
+
+            # Less than k nodes remaining
+            if kth is None:
+                if prev:
+                    prev.next = temp
+                break
+
+            # Save next group
+            next_node = kth.next
+
+            # Separate current k-group
+            kth.next = None
+
+            # Reverse current group
+            prev_node = None
+            curr = temp
+
+            while curr:
+                next_curr = curr.next
+                curr.next = prev_node
+                prev_node = curr
+                curr = next_curr
+
+            # First group -> update head
+            if temp == head:
+                head = kth
+            else:
+                # Connect previous group to current reversed group
+                prev.next = kth
+
+            # temp is now the last node of reversed group
+            prev = temp
+
+            # Move to next group
+            temp = next_node
+
+        return head
