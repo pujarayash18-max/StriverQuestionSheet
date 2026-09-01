f915d469d6f6a0aa99da9d660f73fe7c2ccee9e1
pujarayash18-max <pujarayash18@gmail.com>
2026-09-01 19:51:15 +0530

    Time: 76 ms (76.82%), Space: 39 MB (47.52%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -0,0 +1,43 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        curr = self.root
+
+        for ch in word:
+            if ch not in curr.children:
+                curr.children[ch] = TrieNode()
+
+            curr = curr.children[ch]
+
+        curr.isEnd = True
+
+    def search(self, word):
+        curr = self.root
+
+        for ch in word:
+            if ch not in curr.children:
+                return False
+
+            curr = curr.children[ch]
+
+        return curr.isEnd
+
+    def startsWith(self, prefix):
+        curr = self.root
+
+        for ch in prefix:
+            if ch not in curr.children:
+                return False
+
+            curr = curr.children[ch]
+
+        return True
